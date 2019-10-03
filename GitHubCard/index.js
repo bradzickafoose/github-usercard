a751056375a16caaafccdac1f4d5c645ba922b95
Brad <brad@viziworx.com>
2019-10-03 19:15:46 -0400

    Completed task and stretch

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,6 +3,16 @@
            https://api.github.com/users/<your name>
 */
 
+axios
+  .get('https://api.github.com/users/bradzickafoose')
+  .then(response => {
+    cardsDiv.appendChild(createCard(response.data));
+    console.log(response);
+  })
+  .catch(err => {
+    console.log(err);
+  })
+
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
    data in order to use it to build your component function 
@@ -25,6 +35,27 @@
 */
 
 const followersArray = [];
+const urlArray = [];
+
+axios.get('https://api.github.com/users/bradzickafoose/followers')
+  .then(response => {
+    console.log(response);
+    response.data.forEach((element) => {
+      urlArray.push(`${element.url}`)
+    })
+    urlArray.forEach((element) => {
+      axios.get(element)
+        .then(response => {
+          cardsDiv.appendChild(createCard(response.data));
+        })
+        .catch(err => {
+          console.log(err);
+        })
+    })
+  })
+  .catch(err => {
+    console.log(err);
+})
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -46,10 +77,56 @@ const followersArray = [];
 
 */
 
-/* List of LS Instructors Github username's: 
-  tetondan
-  dustinmyers
-  justsml
-  luishrd
-  bigknell
-*/
+const cardsDiv = document.querySelector('.cards')
+
+function createCard(user) {
+
+  // define new elements
+  const card = document.createElement('div'),
+        img = document.createElement('img'),
+        info = document.createElement('div'),
+        name = document.createElement('h3'),
+        username = document.createElement('p'),
+        location = document.createElement('p'),
+        profile = document.createElement('p'),
+        profileURL = document.createElement('a'),
+        followers = document.createElement('p'),
+        following = document.createElement('p'),
+        repos = document.createElement('p'),
+        bio = document.createElement('p');
+
+  // set text content
+  img.src = user.avatar_url;
+  name.textContent = user.name;
+  username.textContent = user.login;
+  location.textContent = `Location: ${user.location}`;
+  profile.textContent = 'Profile: ';
+  profileURL.href = user.html_url;
+  profileURL.textContent = user.html_url;
+  followers.textContent = `Followers: ${user.followers}`;
+  following.textContent = `Following: ${user.following}`;
+  repos.textContent = `Repositories: ${user.public_repos}`;
+  bio.textContent = user.bio;
+
+  // set structure of elements
+  card.appendChild(img);
+  card.appendChild(info);
+  info.appendChild(name);
+  info.appendChild(username);
+  info.appendChild(location);
+  info.appendChild(profile);
+  info.appendChild(followers);
+  info.appendChild(following);
+  info.appendChild(repos);
+  info.appendChild(bio);
+  profile.appendChild(profileURL);
+
+  // set class names
+  card.classList.add('card');
+  info.classList.add('card-info');
+  name.classList.add('name');
+  username.classList.add('username');
+
+  return card;
+}
+
